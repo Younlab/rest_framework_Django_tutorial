9b092bbc9bb8d5360b59124b798e934ff331c3db
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-23 16:52:45 +0900

    mixin 기법

--- app/config/urls/__init__.py
+++ app/config/urls/__init__.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import django_view
+from . import mixins
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snippets/', include(django_view))
+    path('snippets/', include(django_view)),
+    path('mixin/', include(mixins))
 ]
