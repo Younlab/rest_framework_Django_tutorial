d3256c2a12dbbd6c8c2973cd4c9d3f851de82546
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-23 16:03:05 +0900

    class 로 변경

--- app/config/urls/django_view.py
+++ app/config/urls/django_view.py
@@ -1,9 +1,18 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets.views import django_view
+from snippets.views import api_view as views
 
 app_name = 'snippets'
 
 urlpatterns = [
     path('django_view/snippets/', django_view.snippet_list, name='snippet-list'),
-    path('django_view/snippets/<int:pk>/', django_view.snippet_detail, name='snippet-detail')
+    path('django_view/snippets/<int:pk>/', django_view.snippet_detail, name='snippet-detail'),
+    path('snippets/', views.SnippetList.as_view(),name='snippet-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(),name='snippet-detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+
