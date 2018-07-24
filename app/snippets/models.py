b51f131334e21e2443ebd8f80d33a86d6fd1bb1c
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-24 16:05:33 +0900

    Snippet save()에서 code 를 highlighted에 html로 저장

--- app/snippets/models.py
+++ app/snippets/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -21,3 +23,25 @@ class Snippet(models.Model):
     class Meta:
         ordering = ('created',)
 
+    def save(self, *args, **kwargs):
+        ## 이렇게 하면 html이 저장됩니다.
+        # 지정한 언어(language)에 대한 분석기 (lexer)할당
+        lexer = get_lexer_by_name(self.language)
+
+        # 줄 표시 여부
+        linenos = 'table' if self.linenos else False
+
+        # self.title이 존재하면 options에 'title'키가 들어있는 dict를 전달
+        options = {'title': self.title} if self.title else {}
+
+        # 위에서 지정한 여러 변수를 사용해서 formatter객체 생성
+        formatter = HtmlFormatter(
+            style=self.style,
+            linenos=linenos,
+            full=True,
+            **options,
+        )
+
+
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
